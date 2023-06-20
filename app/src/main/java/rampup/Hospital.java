05ab35bfd5e96866f4eb4a768e434bcbdfdec6d0
Ramzi Rebouh <ramzirebouh@outlook.com>
2023-06-20 16:31:53 +0200

    update agents

--- app/src/main/java/rampup/Hospital.java
+++ app/src/main/java/rampup/Hospital.java
@@ -2,31 +2,58 @@ package rampup;
 
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
 import org.apache.commons.math3.distribution.PoissonDistribution;
 public class Hospital extends Agent {
 
 	int durée; 
 	protected void setup() {
+		
 	System.out.println("Hospital  : "+getLocalName());
-	Object[] args = getArguments() ;
+	Object[] args = getArguments();
 	if (args != null) {
 	durée = Integer.parseInt(args[0].toString());
 	addBehaviour(new Gestion(this, durée));
 	}
-	}
-	public class Gestion extends TickerBehaviour { //comportement Addition
-	public Gestion(Agent ag, int durée){
-	super(ag, durée);
-	}
-	public void onTick(){
-		for(int i=1;i<=5;i++) {
-			double lambda = 10; // mean
-			PoissonDistribution poissonDistribution = new PoissonDistribution(lambda);
-			int randomNumber = poissonDistribution.sample();
-			System.out.println("Hospital "+getLocalName()+" I received : " +randomNumber+ " patients");
+	}	public class Gestion extends TickerBehaviour { //comportement Addition
+		public Gestion(Agent ag, int durée){
+		super(ag, durée);
 		}
+		
+		public void onTick(){
+			for(int i=1;i<=5;i++) {
+				double lambda = 10; // mean
+				PoissonDistribution poissonDistribution = new PoissonDistribution(lambda);
+				int randomNumber = poissonDistribution.sample();
+			}
+	
 
-	} 
+		} 
+    }
+		
+	  public static void main(String[] args) {
+		  
+		    Runtime runtime = Runtime.instance();
+	        Profile profile = new ProfileImpl();
+	        profile.setParameter(Profile.MAIN_HOST, "localhost"); // Set the hostname of the JADE main container
+	        profile.setParameter(Profile.MAIN_PORT, "1099"); // Set the port of the JADE main container
+	        profile.setParameter(Profile.GUI, "true"); // Enable the JADE GUI
+
+	        try {
+	            ContainerController container = runtime.createMainContainer(profile);
+	            AgentController agentController = container.createNewAgent("Hospital", Hospital.class.getName(), null);
+	            agentController.start();
+	        } catch (StaleProxyException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
 	}
 	/*int durée; 
 	protected void setup() {
@@ -56,4 +83,4 @@ public class Hospital extends Agent {
 	        System.out.println("Hospital: " +getAID().getName());
 	    }
 	*/
-}
+
