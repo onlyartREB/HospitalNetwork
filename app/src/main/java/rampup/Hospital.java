8d367204a223eabaf6ef19c7fe5c1d76c2e9049a
Ramzi Rebouh <ramzirebouh@outlook.com>
2023-06-23 16:21:16 +0200

    update agents Trying to create Patients 2

--- app/src/main/java/rampup/Hospital.java
+++ app/src/main/java/rampup/Hospital.java
@@ -11,9 +11,13 @@ import jade.wrapper.StaleProxyException;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
-public class Hospital extends Agent {
 
+
+public class Hospital extends Agent {
+    
 	int durée; 
+	private int capacity = 20;
+	private int activePatients = 0; // Number of currently active patients
 	protected void setup() {
 	
 	System.out.println("Hospital  : "+getLocalName());
@@ -22,34 +26,44 @@ public class Hospital extends Agent {
 	if (args != null) {
 	durée = Integer.parseInt(args[0].toString());
 	*/
-	
-	
+    ContainerController container = getContainerController();
 	addBehaviour(new Gestion(this, 1000));
 	}
 	
-	public class Gestion extends TickerBehaviour { //comportement Addition
+	public class Gestion extends TickerBehaviour { 
 	public Gestion(Agent ag, int durée){
 	super(ag, durée);
 	}
-	public void onTick(){
-        System.out.println("Gestion behaviour executing...");
-		double lambda = 10; // mean
-		PoissonDistribution poissonDistribution = new PoissonDistribution(lambda);
-		int randomNumber = poissonDistribution.sample();
-	      try {
-	    	  for(int i=1;i<=randomNumber;i++) {
-              ContainerController container = getContainerController();
-              String agentName = "Patient" + System.currentTimeMillis();
-              AgentController patient = container.createNewAgent(agentName, "rampup.Patient", null);
-              //AgentController Patient = container.createNewAgent("Patient ", Patient.class.getName(), null);
-              patient.start();
-              System.out.println("Creating " +i);}
-          } catch (StaleProxyException e) {
-              e.printStackTrace();
-          }
-	  }
-	
+	public void onTick() {
+	    System.out.println("Gestion behavior executing...");
+	    double lambda = 10; // mean
+	    PoissonDistribution poissonDistribution = new PoissonDistribution(lambda);
+	    int randomNumber = poissonDistribution.sample();
+
+	    int patientsToCreate = Math.min(randomNumber, capacity - activePatients); // Consider the remaining capacity
+
+	    try {
+	    	System.out.println("Welcome to the " + patientsToCreate + " Patients");
+	        for (int i = 1; i <= patientsToCreate; i++) {
+	            if (activePatients < capacity) {
+	                ContainerController container = getContainerController();
+	                String agentName = "Patient" + System.currentTimeMillis()+ i;;
+	                AgentController patient = container.createNewAgent(agentName, "rampup.Patient", new Object[]{Hospital.this});
+	                patient.start();
+	                activePatients++; // Increment the counter
+	                System.out.println("Creating " + i);
+	            } else {
+	                System.out.println("Hospital reached its capacity. Cannot create more patients.");
+	                break;
+	            }
+	    }} catch (StaleProxyException e) {
+	        e.printStackTrace();
+	    }
 	}
+	}
+	   public void decrementActivePatients() {
+	        activePatients--;
+	    }
 	/*int durée; 
 	protected void setup() {
 	System.out.println("Je suis l'hopital : "+getLocalName());
--- app/src/main/java/rampup/Patient.java
+++ app/src/main/java/rampup/Patient.java
@@ -3,15 +3,36 @@ import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 import jade.domain.introspection.AddedContainer;
 import jade.wrapper.AgentController;
+import jade.core.behaviours.CyclicBehaviour;
 
-public class Patient extends Agent{	 
-	
-	public void setup()
-{ 
-    System.out.println("Patient: " +getAID().getName());
-}
+public class Patient extends Agent {
 
-	protected void takeDown() {
-        System.out.println(getAID().getName() + " : Terminating.");
-}
+    private int lifelos; // LOS of the patient
+    private Hospital hospital; // Reference to the creating Hospital agent
+    
+    
+    public Patient(Hospital hospital) {
+        this.hospital = hospital;
+    }
+    protected void setup() {
+        System.out.println("Patient: " + getLocalName());
+        
+        // Set the lifelos randomly between 1 and 10
+        lifelos = (int) (Math.random() * 10) + 1;
+        addBehaviour(new LifeBehaviour());
+    }
+
+    private class LifeBehaviour extends CyclicBehaviour {
+        public void action() {
+            if (lifelos > 0) {
+                System.out.println("Patient " + getLocalName() + " - Lifespan: " + lifelos);
+                lifelos--;
+            } else {
+                System.out.println("Patient " + getLocalName() + " - Lifespan over. Terminating...");
+                hospital.decrementActivePatients();
+
+                myAgent.doDelete(); // Terminate the agent
+            }
+        }
+        }
 }
