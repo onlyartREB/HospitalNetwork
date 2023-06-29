d8a3ea93504b9057dbda440f4a23a09e125595d4
Ramzi Rebouh <ramzirebouh@outlook.com>
2023-06-29 11:28:16 +0200

    update agents Trying to create Patients in zones

---
+++ app/src/main/java/rampup/Zone.java
@@ -0,0 +1,21 @@
+package rampup;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zone {
+    private List<Hospital> hospitals;
+
+    public Zone() {
+        hospitals = new ArrayList<>();
+    }
+
+    public void addHospital(Hospital hospital) {
+        hospitals.add(hospital);
+    }
+    public int lambda(int time, Hospital hopital) {
+    	int mean = 0; 
+    	
+    	return mean;
+    	
+    }
+}
